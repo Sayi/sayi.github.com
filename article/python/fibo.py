67032c0369b5163c769d8eb65d934bf91df58b61
Sayi <adasai90@gmail.com>
2013-05-30 17:13:03 +0800

    python

---
+++ article/python/fibo.py
@@ -0,0 +1,8 @@
+#! python
+def fib(n) :
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a+b
+    print()
+#print("do it")
---
+++ article/python/helloworld.py
@@ -0,0 +1,9 @@
+#! python
+import sys
+import fibo
+#sys.stdout.write("hello from Python %s\n" % (sys.version,))
+
+def A ():
+	return "helloworld"
+print(A())
+fibo.fib(5)
